320894d787889d71efe152a4cd2e36b028103e60
AntonDot <anton.dotsenko2006@gmail.com>
2025-05-04 00:22:31 +0500

    add anim jump and climbing(crivoe sho capez)

--- The Lost Cat v1/Assets/Scripts/CatMovement.cs
+++ The Lost Cat v1/Assets/Scripts/CatMovement.cs
@@ -6,11 +6,14 @@ public class CatMovement : MonoBehaviour
     public Rigidbody2D rb;
     public float MoveSpeed;
     public Collider2D groundCheck;
+    public Collider2D stairsCheck;
     float HorizontalMovement;
     float VerticalMovement;
 
     private Animator animator;
     private bool m_Grounded;
+    private bool isJumping;
+    private bool isClimbing;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,15 +27,32 @@ public class CatMovement : MonoBehaviour
             m_Grounded = true;
         else
             m_Grounded = false;
-    }
 
+        if (!stairsCheck.IsTouchingLayers(LayerMask.GetMask("Stairs")) && isClimbing)
+        {
+            isClimbing = false;
+            animator.SetBool("IsClimbing", false);
+            rb.gravityScale = 1f;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (isClimbing)
+        {
+            rb.linearVelocityY = VerticalMovement * MoveSpeed;
+            rb.gravityScale = 0;
+        }
         if (HorizontalMovement == 0)
-            animator.SetBool("IsWalking", false);
+            animator.SetFloat("XVelocity", 0f);
         rb.linearVelocity = new Vector2(HorizontalMovement * MoveSpeed, rb.linearVelocityY);
+        if (rb.linearVelocityY < 0 && m_Grounded == true && isJumping == true)
+        {
+            isJumping = false;
+            animator.SetBool("IsJumping", false);
+        }
+
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -40,6 +60,7 @@ public class CatMovement : MonoBehaviour
 
         HorizontalMovement = context.ReadValue<Vector2>().x;
         VerticalMovement = context.ReadValue<Vector2>().y;
+        Debug.Log(HorizontalMovement);
         if (HorizontalMovement < 0)
         {
             HorizontalMovement = -1;
@@ -55,13 +76,24 @@ public class CatMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(rotate);
         }
 
-        if (VerticalMovement > 0 && m_Grounded)
+        if (VerticalMovement > 0 && m_Grounded && !isClimbing)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocityX, MoveSpeed * 1.6f);
-            m_Grounded = false;
-        }
 
-        animator.SetBool("IsWalking", true);
+            if (stairsCheck.IsTouchingLayers(LayerMask.GetMask("Stairs")))
+            {
+                isClimbing = true;
+                animator.SetBool("IsClimbing", true);
+            }
+            else
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, MoveSpeed * 1.6f);
+                m_Grounded = false;
+                isJumping = true;
+                animator.SetBool("IsJumping", true);
+            }
+
+        }
+        animator.SetFloat("XVelocity", 1f);
     }
 
 }
